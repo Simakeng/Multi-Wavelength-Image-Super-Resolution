1f3f6d3ee53597c1e1275fe2e2ec04312aa53787
Simakeng <simakeng@outlook.com>
2023-04-25 13:51:39 +0800

    使用相似其他波段的图像进行超分辨率

--- multi-wavelenth-super-resolution.py
+++ multi-wavelenth-super-resolution.py
@@ -3,8 +3,12 @@ import numpy as np
 
 b_scaled, _, _ = cv2.split(cv2.imread("temp/b_scaled.png"))
 b_scaled = b_scaled.astype(np.float32)
-b_scaled = cv2.dct(b_scaled)
-r = np.zeros((1080,1920))
+b_scaled = cv2.dct(b_scaled) * 8
+# r = np.zeros((1080,1920))
+r, _, _ = cv2.split(cv2.imread("temp/input_r.png"))
+r = r.astype(np.float32)
+r = cv2.dct(r)
 r[0:b_scaled.shape[0],0:b_scaled.shape[1]] = b_scaled
-b_restored = cv2.idct(r) * 8
-cv2.imwrite("temp/b_restored_idct.png", b_restored.astype(np.uint8))
+b_restored = cv2.idct(r)
+
+cv2.imwrite("temp/b_restored_idct_true.png", b_restored.astype(np.uint8))
